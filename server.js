81e446a05f40dba6001deb1b3b07393e4640ba42
iamluisnunez <luis.nunez@mykenzie.snhu.edu>
2024-02-21 22:51:22 -0600

    fixed the styling of the messages so they dont overlap

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const socketIO = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const connectedUsers = new Map();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
@@ -16,15 +17,26 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  //creating a userID
+  const userID = socket.id;
+
+  //add the users to the map
+  connectedUsers.set(userID, socket);
+
+  //broadcast to all channels the a new users joined
+  io.emit("chat message", `${userID} has joined the chat`);
+
   // Listen for incoming messages
   socket.on("chat message", (message) => {
     // Broadcast the message to all connected clients
-    io.emit("chat message", message);
+    io.emit("chat message", `${userID}: ${message}`);
   });
 
   // Handle disconnection
   socket.on("disconnect", () => {
     console.log("User disconnected");
+    connectedUsers.delete(userID);
+    io.emit(`${userID} has disconnected from the chat`);
   });
 });
 app.get("/socket.io/socket.io.js", (req, res) => {
